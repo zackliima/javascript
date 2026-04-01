2e155f6c8a4e627f143a3d3b3818adfb964351cf
zack <isaque.r.lima02@gmail.com>
2026-04-01 00:42:14 -0300

    New Repository - JavaScript
    Update.

---
+++ modulo-a/estudos/ex001/script.js
@@ -0,0 +1,3 @@
+window.alert('My First Message!')  /* É possível utilizar apenas o alert('') */
+window.confirm('Are you enjoying JS?') // Pode se usar comentários assim também
+window.prompt('Whats your name?')
