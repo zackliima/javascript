a30e00b9df78e3f0fde140ee0e7858be608493c9
zack <isaque.r.lima02@gmail.com>
2026-08-23 01:25:38 -0300

    Module C - Lesson 10
    Updating

--- modulo-b/estudos/ex004/script.js
+++ modulo-b/estudos/ex004/script.js
@@ -1,4 +1,4 @@
 var nome = window.prompt(`What's your name?`)
-document.write(`Hello, <strong>${nome}</strong>! Your name has ${nome.length} letters</br>`)
-document.write(`Your name in uppercase is ${nome.toUpperCase()}</br>`)
-document.write(`Your name in lowercase is ${nome.toLowerCase()}</br>`)
+document.write(`Hello, <strong>${nome}</strong>! Your name has ${nome.length} letters`)
+document.writeln(`Your name in uppercase is ${nome.toUpperCase()}`)
+document.writeln(`Your name in lowercase is ${nome.toLowerCase()}`)
