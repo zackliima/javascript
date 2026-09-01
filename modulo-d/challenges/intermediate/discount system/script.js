df87e0f7ee2b227eb509cba96d7aea957f1c03e8
Isaque Lima <isaque.r.lima02@gmail.com>
2026-09-01 17:11:05 -0300

    Module D - Challenges Intermediate
    Updating...

--- modulo-d/challenges/intermediate/discount system/script.js
+++ modulo-d/challenges/intermediate/discount system/script.js
@@ -1,9 +1,9 @@
 function check() {
-    var purchase_value = document.getElementById('purchase_value')
+    var purchase_value = Number(document.getElementById('purchase_value').value)
     var vip = document.getElementsByName('vip')
     var res = document.querySelector('div#res')
 
     if (purchase_value < 0) {
         res.innerHTML = `<strong>Invalid value</strong>`
-    }
+    } 
 }
